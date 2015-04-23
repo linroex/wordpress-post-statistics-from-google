831ed496cb389fac4e7f29d5ece8d487ae96c569
linroex <linroex@coder.tw>
2015-04-23 22:07:47 +0800

    plugin header writed

---
+++ entrance.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Post Statistics
+ * Plugin URI: https://github.com/linroex/wordpress-post-statistics-from-google
+ * Description: Import Google Analytics data in WordPress.
+ * Version: 0.0.1
+ * Author: linroex
+ * Author URI: http://me.coder.tw
+ * License: MIT
+ */

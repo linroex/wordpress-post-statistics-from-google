de26d4d6ed9a4bb68ee725fa13fdd77d720e27c1
linroex <linroex@coder.tw>
2015-04-24 10:33:20 +0800

    edit gitignore

--- entrance.php
+++ entrance.php
@@ -8,3 +8,26 @@
  * Author URI: http://me.coder.tw
  * License: MIT
  */
+include('vendor/autoload.php');
+
+function install() {
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . "statistics"; 
+    $charset = $wpdb->get_charset_collate();
+
+    $sql = "
+        CTEATE TABLE $table_name (
+            id bigint(20) NOT NULL AUTO_INCREMENT,
+            date DATE NOT NULL,
+            post_id bigint(20) NOT NULL,
+            UNIQUE KEY id (id)
+        ) $charset;
+    ";
+
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+
+}
+
